4b76a5e6b95ebe2ec10cb833919985119b56a764
RigidFire63 <144449121+RigidFire63@users.noreply.github.com>
2024-09-07 17:56:07 +0700

    Add files via upload

---
+++ черепашка.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.forward(100)
+turtle.right(90)
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.end_fill()
